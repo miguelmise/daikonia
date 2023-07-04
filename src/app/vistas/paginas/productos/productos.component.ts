ef0768361990a0e9b835a8128136d4ca95e8e9f6
Adagmari <saintria@espol.edu.ec>
2023-07-04 03:54:17 -0500

    Ajuste en Pantalla de producto
    Se modificó los datos del formulario para que se consideren las categorias de los productos

--- src/app/vistas/paginas/productos/productos.component.ts
+++ src/app/vistas/paginas/productos/productos.component.ts
@@ -17,40 +17,15 @@ export class ProductosComponent implements OnInit {
   /**Data de prueba */
   listaProductos: any[] = [
     {
-      "codigo": 1447,
-      "Nombre": "CAJA DE PULP 12 UND DE 1 LTR C/U",
-      "precio": 10.99,
-      "peso": 12.5,
-      "grasa": 5.2,
-      "azucar": 12.8,
-      "fibra": 3.7
-    },
-    {
-      "codigo": 1320,
-      "Nombre": "CAFE INSTANTANEO 40 UND X 50 GR",
-      "precio": 5.99,
-      "peso": 2.0,
-      "grasa": 0.2,
-      "azucar": 0.0,
-      "fibra": 1.0
-    },
-    {
-      "codigo": 28,
-      "Nombre": "CAFE SELLO ROJO 360 G",
-      "precio": 8.49,
-      "peso": 0.36,
-      "grasa": 0.0,
-      "azucar": 0.0,
-      "fibra": 0.0
-    },
-    {
       "codigo": 2683,
       "Nombre": "CAJA AVENA 24 UN DE 150-200 GR",
       "precio": 12.99,
       "peso": 0.175,
       "grasa": 2.5,
       "azucar": 0.5,
-      "fibra": 6.2
+      "fibra": 6.2,
+      "categoriaNombre":"Cereales",
+      "subcategoriaNombre":"Sub Categoria 5"
     },
     {
       "codigo": 2596,
@@ -59,7 +34,9 @@ export class ProductosComponent implements OnInit {
       "peso": 0.375,
       "grasa": 1.0,
       "azucar": 8.5,
-      "fibra": 2.5
+      "fibra": 2.5,
+      "categoriaNombre":"Cereales",
+      "subcategoriaNombre":"Sub Categoria 4"
     },
     {
       "codigo": 3442,
@@ -68,7 +45,9 @@ export class ProductosComponent implements OnInit {
       "peso": 9.07,
       "grasa": 0.3,
       "azucar": 15.6,
-      "fibra": 7.9
+      "fibra": 7.9,
+      "categoriaNombre":"Fruta",
+      "subcategoriaNombre":"Sub Categoria 3"
     },
     {
       "codigo": 2709,
@@ -77,7 +56,9 @@ export class ProductosComponent implements OnInit {
       "peso": 0.12,
       "grasa": 3.8,
       "azucar": 4.2,
-      "fibra": 0.5
+      "fibra": 0.5,
+      "categoriaNombre":"Proteínas",
+      "subcategoriaNombre":"Sub Categoria 1"
     },
     {
       "codigo": 2985,
@@ -86,7 +67,9 @@ export class ProductosComponent implements OnInit {
       "peso": 0.675,
       "grasa": 2.0,
       "azucar": 12.5,
-      "fibra": 0.0
+      "fibra": 0.0,
+      "categoriaNombre":"Lácteos",
+      "subcategoriaNombre":"Sub Categoria 6"
     },
     {
       "codigo": 2159,
@@ -95,8 +78,52 @@ export class ProductosComponent implements OnInit {
       "peso": 0.975,
       "grasa": 90.0,
       "azucar": 0.0,
-      "fibra": 0.0
+      "fibra": 0.0,
+      "categoriaNombre":"Grasas",
+      "subcategoriaNombre":"Sub Categoria 7"
+    },
+    {
+      "codigo": 1447,
+      "Nombre": "CAJA DE PULP 12 UND DE 1 LTR C/U",
+      "precio": 10.99,
+      "peso": 12.5,
+      "grasa": 5.2,
+      "azucar": 12.8,
+      "fibra": 3.7,
+      "categoriaNombre":"Grasas",
+      "subcategoriaNombre":"Sub Categoria 7"
+    },
+    {
+      "codigo": 1320,
+      "Nombre": "CAFE INSTANTANEO 40 UND X 50 GR",
+      "precio": 5.99,
+      "peso": 2.0,
+      "grasa": 0.2,
+      "azucar": 0.0,
+      "fibra": 1.0,
+      "categoriaNombre":"Grasas",
+      "subcategoriaNombre":"Sub Categoria 7"
+    },
+    {
+      "codigo": 28,
+      "Nombre": "CAFE SELLO ROJO 360 G",
+      "precio": 8.49,
+      "peso": 0.36,
+      "grasa": 0.0,
+      "azucar": 0.0,
+      "fibra": 0.0,
+      "categoriaNombre":"Grasas",
+      "subcategoriaNombre":"Sub Categoria 7"
     }
+    
+  ];
+  subcategoriaProductos: any[] = [
+    {id: 1, subcategoriaNombre:"Sub Categoria 1",categoriaNombre:"Proteínas", descripcion:"Descripcion Sub Categoria 1"},
+    {id: 2, subcategoriaNombre:"Sub Categoria 2",categoriaNombre:"Proteínas", descripcion:"Descripcion Sub Categoria 2"},
+    {id: 3, subcategoriaNombre:"Sub Categoria 3",categoriaNombre:"Frutas", descripcion:"Descripcion Sub Categoria 3"},
+    {id: 4, subcategoriaNombre:"Sub Categoria 4",categoriaNombre:"Cereales", descripcion:"Descripcion Sub Categoria 4"},
+    {id: 5, subcategoriaNombre:"Sub Categoria 5",categoriaNombre:"Cereales", descripcion:"Descripcion Sub Categoria 5"},
+    {id: 6, subcategoriaNombre:"Sub Categoria 6",categoriaNombre:"Lácteos", descripcion:"Descripcion Sub Categoria 6"}
   ];
 
   displayedColumns: string[] = ['producto', 'peso'];
@@ -112,16 +139,22 @@ export class ProductosComponent implements OnInit {
   submitted = false;
   isFormVisible: boolean = false;
   selectProducto = "";
+  selected_categoria:any="";
 
   constructor(private cdr: ChangeDetectorRef, private formBuilder: FormBuilder, private _util: UtilService, private _producto : ProductosService) { 
     this.registerForm = this.formBuilder.group({
       id_producto: [""],
       nombre_producto: ["",Validators.required],
       precio: ["",Validators.required],
+      grasa: [""],
+      azucar: [""],
+      fibra: [""],
       peso: ["",Validators.required],
-      grasa: ["",Validators.required],
+      /*grasa: ["",Validators.required],
       azucar: ["",Validators.required],
-      fibra: ["",Validators.required]
+      fibra: ["",Validators.required],*/
+      subcategoria_Nombre:["",Validators.required],
+      categoria_Nombre:[""]
     });
   }
 
@@ -130,10 +163,10 @@ export class ProductosComponent implements OnInit {
     this.cdr.detectChanges();
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
-    this.cargarListaProductos();
+    //this.cargarListaProductos();
   }
 
-  cargarListaProductos():void{
+  /*cargarListaProductos():void{
     this._producto.listar_productos().subscribe({
       next: res=>{
         console.log(res)
@@ -142,7 +175,7 @@ export class ProductosComponent implements OnInit {
         console.log(err)
       }
     })
-  }
+  }*/
 
   resetForm():void{
     this.selectProducto = "[Nuevo Producto]"
@@ -150,6 +183,8 @@ export class ProductosComponent implements OnInit {
     this.submitted = false;
     this.registerForm.reset();
   }
+  
+  
 
   onSubmit():void {
     this.guardarDatosProducto()
@@ -189,6 +224,9 @@ export class ProductosComponent implements OnInit {
       this.registerForm.controls["grasa"].setValue(producto.grasa)
       this.registerForm.controls["azucar"].setValue(producto.azucar)
       this.registerForm.controls["fibra"].setValue(producto.fibra)
+      this.registerForm.controls["categoria_Nombre"].setValue(producto.categoriaNombre)
+      this.registerForm.controls["subcategoria_Nombre"].setValue(producto.subcategoriaNombre)
+      
     } else {
       this._util.alerta("Error","No se encontro la información del Producto.","warning")
     }
